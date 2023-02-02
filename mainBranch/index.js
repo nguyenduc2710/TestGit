4fd818cf187cdd8563fa055cc6a87817379e0656
NHHD-Yamj <hongducnguyenho0@gmail.com>
2023-02-02 13:03:18 +0700

    add babel cdn, and add a space on index.js

--- mainBranch/index.js
+++ mainBranch/index.js
@@ -2,6 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Header from "../subBranch/header"
 
-ReactDOM.render(<Header/>, document.getElementById('root'))
+ReactDOM.render(<Header />, document.getElementById('root'))
 
 
